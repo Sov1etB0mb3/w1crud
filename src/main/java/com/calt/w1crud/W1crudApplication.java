545971f40a0a25bfecf03d16fb2552e9228afcc9
Khanh Nguyen Trung <khanhnt@imt-soft.com>
2026-03-04 09:45:33 +0700

    first commit

---
+++ src/main/java/com/calt/w1crud/W1crudApplication.java
@@ -0,0 +1,13 @@
+package com.calt.w1crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1crudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W1crudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calt/w1crud/W1crudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calt.w1crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W1crudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
