839dcae9e6b1120b182c3edfa4ec019a00543e25
Khanh Nguyen Trung <khanhnt@imt-soft.com>
2026-03-06 16:50:39 +0700

    fix CategoryService

--- src/main/java/com/calt/w1crud/coffeestore/Service/CategoryService.java
+++ src/main/java/com/calt/w1crud/coffeestore/Service/CategoryService.java
@@ -4,6 +4,7 @@ import com.calt.w1crud.coffeestore.Controller.GlobalExceptionHandler;
 import com.calt.w1crud.coffeestore.DTO.RequestCategory;
 import com.calt.w1crud.coffeestore.Entity.Category;
 import com.calt.w1crud.coffeestore.Repository.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.util.Optional;
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
     //no need to add autowire because there is only way to inject is through constructor with only one parameter
     public CategoryService(CategoryRepository categoryRepository){
         this.categoryRepository=categoryRepository;
@@ -38,12 +40,11 @@ public class CategoryService {
         return null;
 
     }
+    @Transactional
     public void deleteCategory(Long id){
-       try{
+
            categoryRepository.delete(getCategoryByID(id));
-       } catch (DataIntegrityViolationException e) {
-           throw new DataIntegrityViolationException("Can't delete");
-       }
+
     }
 
 
